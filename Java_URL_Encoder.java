edb92744f7bad31846ddba81fa443e66320a09d4
winder24962 <106226709+winder24962@users.noreply.github.com>
2023-11-02 00:58:13 +0900

    Add files via upload

---
+++ Java_URL_Encoder.java
@@ -0,0 +1,13 @@
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class urls {
+    public static void main(String[] args) throws Exception {
+        String source = "index&^%*a test for 2.5% and  &";
+        String encoded = URLEncoder.encode(source, "utf-8");
+        String decoded = URLDecoder.decode(encoded, "utf-8");
+        System.out.println("Source: "+ source);
+        System.out.println("Encoded: "+ encoded);
+        System.out.println("Decoded: "+ decoded);
+    }
+}
